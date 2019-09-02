6b338a568d79b911a4a7a1e7c3b2ebf87e4685e2
asengine <asengine@qq.com>
2019-09-02 18:11:51 +0800

    Signed-off-by: asengine <asengine@qq.com>

---
+++ src/app/index/index.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index.component';
+import { IndexRoutingModule } from './index.routing.module';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
+
+
+@NgModule({
+  declarations: [IndexComponent],
+  imports: [
+    CommonModule,
+    NgZorroAntdModule,
+    IndexRoutingModule
+  ]
+})
+export class IndexModule { }
