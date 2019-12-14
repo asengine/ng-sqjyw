0d4f2900a3b84a4ecdb35707b5c709d643d81156
asengine <asengine@qq.com>
2019-12-14 23:28:06 +0800

    Signed-off-by: asengine <asengine@qq.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   SERVER_URL: `./`,
   production: true,
-  AUTH_URL: 'http://192.168.1.200:30015/', //客户端身份认证接口服务
-  RSJ_URL: 'http://192.168.1.200:30016/', //远程人社局接口服务
-  JYW_URL: 'http://192.168.1.200:30017/', //远程就业网接口服务
+  AUTH_URL: 'http://192.168.2.31:30015/', //客户端身份认证接口服务
+  RSJ_URL: 'http://192.168.2.31:30016/', //远程人社局接口服务
+  JYW_URL: 'http://192.168.2.31:30017/', //远程就业网接口服务
   RDC_URL: 'http://localhost:8018/', //读取社保卡及本地MAC服务
   IDC_URL: 'http://localhost:8989/' //客户端读卡器服务
 };
