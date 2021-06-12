a953792c67a3939b5d7d723a1e553743edd49dcc
asengine <asengine@qq.com>
2021-06-12 13:39:53 +0800

    2
    2

---
+++ src/app/core/models/jiuguanzhongxin/DanWeiXiNaJiuYeKunNanRenYuanSheBaoBuTie.ts
@@ -0,0 +1,24 @@
+
+/**
+ * 单位吸纳就业困难人员社保补贴
+ */
+export class DanWeiXiNaJiuYeKunNanRenYuanSheBaoBuTie {
+
+    bac001: string = '个人编号';
+    bab001: string = '单位编号';
+    aab004: string = '单位名称';
+    aac003: string = '姓名';
+    aac147: string = '身份证号';
+    aae010: string = '银行账号';
+    aac067: string = '联系人电话';
+    bcca14: string = '人员类别';
+    aac010: string = '户籍地址';
+    adc450: string = '补贴类别';
+    adc456: string = '已享受月数';
+    abc921: string = '补贴年限';
+    aae036: string = '申请时间';
+    bcca14name: string = '人员类别名称';
+    adc450name: string = '补贴类别名称';
+
+    constructor() { }
+}
